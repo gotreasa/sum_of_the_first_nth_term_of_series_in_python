c836779e6a297c76329b02cadfdea67301f2aada
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-02-06 16:16:42 +0000

    refactor: extract denominator into a function

--- modules/nth_sum.py
+++ modules/nth_sum.py
@@ -1,9 +1,10 @@
+def calculate_denominator(value):
+    return 1 if value == 1 else (value - 1) * 3 + 1
+
 def series_sum(number: int) -> float:
     if not isinstance(number, int):
         raise ValueError("❗️ Input should be a number")
     result = 0
     for value in range(1, number + 1):
-        print(value)
-        denominator = 1 if value == 1 else (value - 1) * 3 + 1
-        result += 1 / denominator
+        result += 1 / calculate_denominator(value)
     return round(result, 2)
