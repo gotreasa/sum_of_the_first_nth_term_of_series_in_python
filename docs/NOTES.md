47b66b4f9a1088f0f9028fbd1e9ceb3befd92f85
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-02-06 16:14:40 +0000

    refactor: programmatically calculate the sum of the series

--- docs/NOTES.md
+++ docs/NOTES.md
@@ -13,4 +13,4 @@
 - ✅ should return an error if the input is not a number
 - ✅ should return 1.00 for the input 1
 - ✅ should return 1.25 for the input 2
-- 🚧 should return 1.57 for the input 5
+- ✅ should return 1.57 for the input 5
--- modules/nth_sum.py
+++ modules/nth_sum.py
@@ -1,8 +1,9 @@
 def series_sum(number: int) -> float:
-    if number == 1:
-        return 1.00
-    if number == 2:
-        return 1.25
-    if number == 5:
-        return 1.57
-    raise ValueError("❗️ Input should be a number")
+    if not isinstance(number, int):
+        raise ValueError("❗️ Input should be a number")
+    result = 0
+    for value in range(1, number + 1):
+        print(value)
+        denominator = 1 if value == 1 else (value - 1) * 3 + 1
+        result += 1 / denominator
+    return round(result, 2)
